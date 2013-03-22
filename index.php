f8648de65ba9ce40c2fe6c338775e6bba02a4a0e
Screeny <screeny05@gmail.com>
2013-03-22 16:38:32 +0100

    basic folder-structure and index.php

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>SynergyTube</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/assets/css/footer.css" rel="stylesheet">
+</head>
+<body>
+  <div class="navbar navbar-fixed-top navbar-inverse">
+    <div class="navbar-inner">
+      <div class="container">
+        <a class="brand" href="/">SynergyTube</a>
+        <ul class="nav">
+          <li class="active"><a href="/">Channels</a></li>
+          <li><a href="/categories">Categories</a></li>
+        </ul>
+        <ul class="nav pull-right">
+          <li><a href="#">Log In</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <div id="wrap">
+    <div class="container content">
+      <!--<p>SynergyTube is a foolish attempt on successing the now shut-down synchtube. We will try hard to keep the experience here as close as possible to the original while also following our own ideas.</p>-->
+      <div class="page-header"><h1>Browse all Channels</h1></div>
+      <ul class="thumbnails">
+
+        <li class="span4">
+          <div class="thumbnail">
+            <img class="pull-right" src="http://placehold.it/64" alt="">
+            <h4><a href="#">BroniesBW</a></h4>
+            <p>Official German Pony-Music Channel from BroniesBW!<br><small class="muted">Viewers: 127 | Favourites: 981</small></p>
+            <div class="img-wrap">
+              <a href="#"><img src="https://img.youtube.com/vi/Sob1t-iUCmE/mqdefault.jpg" alt=""></a>
+              <div class="img-overlay">
+                <p><small>Now on:</small><br>Epic Wub Time: Musicians of Ponyville</p>
+              </div>
+            </div>
+          </div>
+        </li>
+
+        <li class="span4">
+          <div class="thumbnail">
+            <img class="pull-right" src="http://placehold.it/64" alt="">
+            <h4><a href="#">IndustrialUnited</a></h4>
+            <p>From A like Agonoize to W like Wumpscut, we play everything Industrial!<br><small class="muted">Viewers: 80 | Favourites: 811</small></p>
+            <div class="img-wrap">
+              <a href="#"><img src="https://i4.ytimg.com/vi/cNAdtkSjSps/mqdefault.jpg" alt=""></a>
+              <div class="img-overlay">
+                <p><small>Now on:</small><br>Eisenfunk - Pong</p>
+              </div>
+            </div>
+          </div>
+        </li>
+
+        <li class="span4">
+          <div class="thumbnail">
+            <img class="pull-right" src="http://placehold.it/64" alt="">
+            <h4><a href="#">ChadVaderTV</a></h4>
+            <p>We play Chad Vader - Staff Manager all the Time and never get bored!<br><small class="muted">Viewers: 147 | Favourites: 760</small></p>
+            <div class="img-wrap">
+              <a href="#"><img src="https://i4.ytimg.com/vi/Stz3VHSQg44/mqdefault.jpg" alt=""></a>
+              <div class="img-overlay">
+                <p><small>Now on:</small><br>Chad Vader Season 3 Episode 6</p>
+              </div>
+            </div>
+          </div>
+        </li>
+        
+      </ul>
+    </div>
+    <div id="push"></div>
+  </div>
+
+  <div id="footer">
+    <div class="container fContainer">
+      <p>&copy; SynergyTube by Screeny05; Fork me on <a href="#">GitHub</a></p>
+    </div>
+  </div>
+
+
+
+  <script src="../assets/js/jquery.js"></script>
+  <script src="../assets/js/bootstrap.js"></script>
+</body>
+</html>
