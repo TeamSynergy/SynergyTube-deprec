3eba0d948a576944c3510f98b6ef21b8763b0753
Screeny <screeny05@gmail.com>
2013-03-22 19:08:28 +0100

    Minor Layout Changes
    - renamed footer.css to custom.css
    - added jQuery
    - redesigned Channel-View
    - switched to BootstrapCDN
    - replaced gradient-background with a flat one

--- index.php
+++ index.php
@@ -4,87 +4,98 @@
   <meta charset="utf-8">
   <title>SynergyTube</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
-  <link href="/assets/css/footer.css" rel="stylesheet">
+  <link href="//netdna.bootstrapcdn.com/bootswatch/2.3.1/cyborg/bootstrap.min.css" rel="stylesheet">
+  <link href="//netdna.bootstrapcdn.com/font-awesome/3.0.2/css/font-awesome.css" rel="stylesheet">
+  <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-responsive.min.css" rel="stylesheet">
+  <link href="/assets/css/custom.css" rel="stylesheet">
 </head>
 <body>
   <div class="navbar navbar-fixed-top navbar-inverse">
     <div class="navbar-inner">
       <div class="container">
+        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </a>
+
         <a class="brand" href="/">SynergyTube</a>
-        <ul class="nav">
-          <li class="active"><a href="/">Channels</a></li>
-          <li><a href="/categories">Categories</a></li>
-        </ul>
-        <ul class="nav pull-right">
-          <li><a href="#">Log In</a></li>
-        </ul>
+        
+        <div class="nav-collapse collapse">
+          <ul class="nav">
+            <li class="active"><a href="/">Channels</a></li>
+            <li><a href="/categories">Categories</a></li>
+          </ul>
+          <ul class="nav pull-right">
+            <li><a href="#">Log In</a></li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
 
-  <div id="wrap">
+  <div class="content-wrap">
     <div class="container content">
       <!--<p>SynergyTube is a foolish attempt on successing the now shut-down synchtube. We will try hard to keep the experience here as close as possible to the original while also following our own ideas.</p>-->
       <div class="page-header"><h1>Browse all Channels</h1></div>
-      <ul class="thumbnails">
+      <div class="row-fluid">
 
-        <li class="span4">
-          <div class="thumbnail">
-            <img class="pull-right" src="http://placehold.it/64" alt="">
+        <div class="span4 channel-thumb">
+          <div class="txt-wrap">
             <h4><a href="#">BroniesBW</a></h4>
-            <p>Official German Pony-Music Channel from BroniesBW!<br><small class="muted">Viewers: 127 | Favourites: 981</small></p>
-            <div class="img-wrap">
-              <a href="#"><img src="https://img.youtube.com/vi/Sob1t-iUCmE/mqdefault.jpg" alt=""></a>
-              <div class="img-overlay">
-                <p><small>Now on:</small><br>Epic Wub Time: Musicians of Ponyville</p>
-              </div>
+            <p>Official German Pony-Music Channel from BroniesBW! We play everything from Ponystep to Ponywhat.<br>
+            <small class="muted">Viewers: 127 | Favourites: 981</small></p>
+          </div>
+          <div class="img-wrap">
+            <a href="#"><img src="https://img.youtube.com/vi/Sob1t-iUCmE/mqdefault.jpg" alt=""></a>
+            <div class="img-overlay">
+              <p><small class="muted">Now on:</small><br>Epic Wub Time: Musicians of Ponyville</p>
             </div>
           </div>
-        </li>
+        </div>
 
-        <li class="span4">
-          <div class="thumbnail">
-            <img class="pull-right" src="http://placehold.it/64" alt="">
+        <div class="span4 channel-thumb">
+          <div class="txt-wrap">
             <h4><a href="#">IndustrialUnited</a></h4>
-            <p>From A like Agonoize to W like Wumpscut, we play everything Industrial!<br><small class="muted">Viewers: 80 | Favourites: 811</small></p>
-            <div class="img-wrap">
-              <a href="#"><img src="https://i4.ytimg.com/vi/cNAdtkSjSps/mqdefault.jpg" alt=""></a>
-              <div class="img-overlay">
-                <p><small>Now on:</small><br>Eisenfunk - Pong</p>
-              </div>
+            <p>From A like Agonoize to W like Wumpscut, we play everything Industrial! Enjoy brand new Tracks and also classics. Lorem Ipsum Dolor Sit Amet<br>
+            <small class="muted">Viewers: 127 | Favourites: 981</small></p>
+          </div>
+          <div class="img-wrap">
+            <a href="#"><img src="https://i4.ytimg.com/vi/cNAdtkSjSps/mqdefault.jpg" alt=""></a>
+            <div class="img-overlay">
+              <p><small class="muted">Now on:</small><br>Eisenfunk - Pong</p>
             </div>
           </div>
-        </li>
+        </div>
 
-        <li class="span4">
-          <div class="thumbnail">
-            <img class="pull-right" src="http://placehold.it/64" alt="">
+        <div class="span4 channel-thumb">
+          <div class="txt-wrap">
             <h4><a href="#">ChadVaderTV</a></h4>
-            <p>We play Chad Vader - Staff Manager all the Time and never get bored!<br><small class="muted">Viewers: 147 | Favourites: 760</small></p>
-            <div class="img-wrap">
-              <a href="#"><img src="https://i4.ytimg.com/vi/Stz3VHSQg44/mqdefault.jpg" alt=""></a>
-              <div class="img-overlay">
-                <p><small>Now on:</small><br>Chad Vader Season 3 Episode 6</p>
-              </div>
+            <p>We play Chad Vader - Staff Manager all the Time and never get bored! Find us on Facebook!<br>
+            <small class="muted">Viewers: 127 | Favourites: 981</small></p>
+          </div>
+          <div class="img-wrap">
+            <a href="#"><img src="https://i4.ytimg.com/vi/Stz3VHSQg44/mqdefault.jpg" alt=""></a>
+            <div class="img-overlay">
+              <p><small class="muted">Now on:</small><br>Epic Wub Time: Musicians of Ponyville</p>
             </div>
           </div>
-        </li>
+        </div>
         
-      </ul>
+      </div>
     </div>
-    <div id="push"></div>
+    <div class="footer-pusher"></div>
   </div>
 
-  <div id="footer">
-    <div class="container fContainer">
+  <div class="footer">
+    <div class="container footer-container">
       <p>&copy; SynergyTube by Screeny05; Fork me on <a href="#">GitHub</a></p>
     </div>
   </div>
 
 
 
-  <script src="../assets/js/jquery.js"></script>
-  <script src="../assets/js/bootstrap.js"></script>
+  <script src="/assets/js/jquery.min.js"></script>
+  <script src="/assets/js/bootstrap.min.js"></script>
 </body>
 </html>
