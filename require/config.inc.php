b4ba79ca069a80e03680eebcba99c29414c07139
Screeny <screeny05@gmail.com>
2013-05-04 02:00:18 +0200

    Merge with ae30d0d

--- require/config.inc.php
+++ require/config.inc.php
@@ -2,14 +2,14 @@
 	// Database-Connection variables
 	$db_host = "localhost";
 	$db_user = "root";
-	$db_password = "root";
-	$db_table = "synergy";
+	$db_password = "";
+	$db_table = "test";
 
 	// Use mod_rewrite for url rewriting?
 	// eg: host.com/c/channel-name instead of host.com/channe.php?c=channel-name when enabled.
 	$enable_mod_rewrite = true;
 
 	// Don't even think about touching this Values!
-	$d_host = '//'.$_SERVER['SERVER_NAME'];
-	$d_root = $d_host.substr($_SERVER['SCRIPT_NAME'],0,strrpos($_SERVER['SCRIPT_NAME'],'/'));
+  $sgtube_host = '//'.$_SERVER['SERVER_NAME'];
+  $sgtube_root = $sgtube_host.substr($_SERVER['SCRIPT_NAME'],0,strrpos($_SERVER['SCRIPT_NAME'],'/'));
 ?>
