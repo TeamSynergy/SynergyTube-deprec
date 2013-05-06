f3e8da40eee18a74f921b006e407250dacfef949
Jonas K., Esq. <jonas@derp.dk>
2013-05-06 04:06:08 +0200

    #126 fix - Next level retarded
    This was making me pull my hair out...

--- socket_server/mysql_backend.js
+++ socket_server/mysql_backend.js
@@ -89,7 +89,7 @@ exports.createStructure = function(db_config, callback){
 	
 	c("CREATE TABLE IF NOT EXISTS tblUser(_id int(11) NOT NULL AUTO_INCREMENT, login_name varchar(45) NOT NULL, display_name varchar(45) NOT NULL, " +
 		"email varchar(90) NOT NULL, avatar_id varchar(45) DEFAULT NULL, strategy varchar(10) NOT NULL, hash varchar(200) NOT NULL, session_id char(64) DEFAULT NULL, " +
-		"is_valid tinyint(1) NOT NULL, validate_hash char(60) NOT NULL, PRIMARY KEY(_id), UNIQUE KEY login_name_UNIQUE (login_name), UNIQUE KEY email_UNIQUE (email), " +
+		"is_valid tinyint(1) NOT NULL, validate_hash char(64) NOT NULL, PRIMARY KEY(_id), UNIQUE KEY login_name_UNIQUE (login_name), UNIQUE KEY email_UNIQUE (email), " +
 		"UNIQUE KEY display_name_UNIQUE (display_name)) DEFAULT CHARSET=utf8",function(){
 		console.log("created table tblUser");
 	callback(0);
